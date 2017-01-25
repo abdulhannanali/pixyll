1852807bb31fc631c3e16cc1d49d45b7926fe6f5
Hannan Ali <abdulhannanali@outlook.com>
2017-01-25 18:20:41 +0500

    modify surge file

--- scripts/deploy-surge.sh
+++ scripts/deploy-surge.sh
@@ -3,8 +3,8 @@ set -e
 
 SOURCE_BRANCH="master"
 
-if ["$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH"]; then
-    echo "Skipping deploy"
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
     exit 0
 fi
 
