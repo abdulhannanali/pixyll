8a12fcc1f858dda8e8368583ce1890d37ed625ff
Hannan Ali <abdulhannanali@outlook.com>
2017-01-25 18:13:52 +0500

    add more detailed surge script

--- scripts/deploy-surge.sh
+++ scripts/deploy-surge.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-surge
+SOURCE_BRANCH="master"
+
+if ["$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH"]; then
+    echo "Skipping deploy"
+    exit 0
+fi
+
+echo "Deploying to surge"
+surge --domain $STAGING_SURGE --project ./_site
